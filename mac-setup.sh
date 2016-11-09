6555e1103d00275066072f79dbafe5cc8e3b4d4c
Dennis Hall <hall.dennis+github@gmail.com>
2016-11-09 10:07:47 -0500

    Create mac-setup.sh

---
+++ mac-setup.sh
@@ -0,0 +1,21 @@
+# create bash_profile
+touch ~/.bash_profile
+
+# install homebrew
+
+# install nvm
+
+# install nodejs
+
+# install java
+
+# set terminal
+
+# OPTIONAL
+# install chrome
+# install intellij
+# install sublime text
+# install atom
+# install brackets
+# install Charles proxy
+# install virtualbox or vmware
