1411d4e39201c339524c2fba17b62381115d8dc5
Dennis Hall <hall.dennis+github@gmail.com>
2016-11-09 10:52:18 -0500

    Update mac-setup.sh

--- mac-setup.sh
+++ mac-setup.sh
@@ -4,12 +4,25 @@ touch ~/.bash_profile
 # install homebrew
 
 # install nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
 
 # install nodejs
+nvm i latest
 
 # install java
 
 # set terminal
+# https://github.com/stephenway/monokai.terminal
+# set width to 180, height to 40 rows
+
+# If ~./inputrc doesn't exist yet, first include the original /etc/inputrc so we don't override it
+if [ ! -a ~/.inputrc ]; then echo '$include /etc/inputrc' > ~/.inputrc; fi
+
+# Add option to ~/.inputrc to enable case-insensitive tab completion
+echo 'set completion-ignore-case On' >> ~/.inputrc
+
+
+# install slack, whatsapp, viber
 
 # OPTIONAL
 # install chrome
